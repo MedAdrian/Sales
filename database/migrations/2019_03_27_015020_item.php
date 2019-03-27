3edbecd8f4fae5c902202334e9fef7aa2150b2fc
Adrian Vera <adrian-860@hotmail.com>
2019-03-26 21:59:11 -0500

    feature: Data seed migration
    Setting the initial migration data for the proyect.

---
+++ database/migrations/2019_03_27_015020_item.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Item extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->double('price');
+            $table->integer('stock');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
